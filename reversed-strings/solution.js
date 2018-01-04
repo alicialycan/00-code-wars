00482e95273177826a6b0e50e3dc9005b44a14ea
Alicia Lycan <alicialycan@gmail.com>
2018-01-03 20:04:03 -0800

    kata dry solution

--- reversed-strings/solution.js
+++ reversed-strings/solution.js
@@ -4,3 +4,9 @@ function solution(str){
     var array = str.split('');
     return array.reverse().join('');
   }
+
+  // or another option:
+
+  function solution(str){
+    return str.split('').reverse().join('');  
+  }
